863971578f2c7e0a9b2c28ac1242c7e4131a0e74
Love Westlund <love.westlund@proton.me>
2023-03-03 22:51:46 +0100

    Add charge threshold tmpfiles

--- install.sh
+++ install.sh
@@ -14,4 +14,6 @@ ln -sf $repo/bin/* ~/.local/bin/
 
 sudo cp $repo/xorg/* /etc/X11/xorg.conf.d/
 
+sudo cp $repo/etc/tmpfiles.d/charge_threshold.conf /etc/tmpfiles.d/
+
 ln -sf $repo/clang-format   $HOME/.clang-format
