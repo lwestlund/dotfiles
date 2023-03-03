a6dfa8b251344ea1a66a418ee317d99e9ef7e37e
Love Westlund <love.westlund@proton.me>
2023-03-03 22:53:38 +0100

    hyprland: Add config - we are Wayland now

---
+++ config/hypr/lock.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env sh
+
+swaylock \
+    --image ~/Pictures/wallpaper.jpg \
+    --font "SF Pro Display" \
+    --font-size 22 \
+    --indicator-idle-visible \
+    --indicator-radius 100 \
+    --inside-color 014c00aa
