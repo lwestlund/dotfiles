98b9b47c790a53201048ec768605296e87728481
Love Westlund <love.westlund@proton.me>
2023-03-29 08:27:07 +0200

    install: Change to explicit linking of configs

--- install.sh
+++ install.sh
@@ -7,7 +7,22 @@ repo=$(readlink -f $rel_dir)
 readonly repo
 
 ln -sf $repo/zshenv     ~/.zshenv
-ln -sf $repo/config/*   ~/.config/
+
+function install_config() {
+    function link_config() {
+        ln -sf $repo/config/$1 ~/.config/$(dirname $1)/
+    }
+    link_config alacritty
+    link_config bat
+    link_config git
+    link_config hypr
+    link_config latexmk
+    link_config mako
+    link_config nvim
+    link_config waybar
+    link_config zsh
+}
+install_config
 
 mkdir -p ~/.local/bin
 ln -sf $repo/bin/* ~/.local/bin/
