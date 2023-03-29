2e2a340f2503543198d5b603765439dc770f859e
Love Westlund <love.westlund@proton.me>
2023-03-29 08:40:32 +0200

    xorg: No need to setup config when using Wayland

--- install.sh
+++ install.sh
@@ -36,8 +36,6 @@ systemctl --user enable waybar.service
 mkdir -p ~/.local/bin
 ln -sf $repo/bin/* ~/.local/bin/
 
-sudo cp $repo/xorg/* /etc/X11/xorg.conf.d/
-
 sudo cp $repo/etc/tmpfiles.d/charge_threshold.conf /etc/tmpfiles.d/
 
 ln -sf $repo/clang-format   $HOME/.clang-format
