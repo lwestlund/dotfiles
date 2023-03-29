5c8893ee034c8657a787eb0ac121f902e3b3b98c
Love Westlund <love.westlund@proton.me>
2023-03-29 08:58:04 +0200

    hypr: Update lock screen configuration

--- config/hypr/lock.sh
+++ config/hypr/lock.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env sh
 
 swaylock \
-    --image ~/Pictures/wallpaper.jpg \
+    --image ~/Pictures/lock_screen.jpg \
     --font "SF Pro Display" \
-    --font-size 22 \
-    --indicator-idle-visible \
-    --indicator-radius 100 \
-    --inside-color 014c00aa
+    --font-size 16 \
+    --indicator-radius 40 \
+    --indicator-thickness 8 \
+    --indicator-x-position 200 \
+    --indicator-y-position 880 \
+    --inside-color 014c00aa \
+    --layout-bg-color 01010177
