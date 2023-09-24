cd2012d5c19350428f02a8e96653330f54db20cc
Love Westlund <love.westlund@proton.me>
2023-09-24 10:44:09 +0200

    Add consult-line search first match to evil-search
    This makes it possible to `consult-line`-search something and then go to
    next/previous match (first search component only) with `n`/`N` as you
    expect in vim.

--- config.el
+++ config.el
@@ -46,6 +46,20 @@
 (after! magit
   (magit-delta-mode +1))
 
+;; From https://github.com/minad/consult/issues/318#issuecomment-882067919
+(defun add-consult-line-to-evil-history (&rest _)
+  "Add latest `consult-line' search pattern to the evil search history ring.
+   This only works with orderless and for the first component of the search."
+  (when (and (bound-and-true-p evil-mode)
+             (eq evil-search-module 'evil-search))
+    (let ((pattern (car (orderless-pattern-compiler (car consult--line-history)))))
+      (add-to-history 'evil-ex-search-history pattern)
+      (setq evil-ex-search-pattern (list pattern t t))
+      (setq evil-ex-search-direction 'forward)
+      (when evil-ex-search-persistent-highlight
+        (evil-ex-search-activate-highlight evil-ex-search-pattern)))))
+(advice-add #'consult-line :after #'add-consult-line-to-evil-history)
+
 ;;; :lang org
 (setq +org-roam-auto-backlinks-buffer nil
       org-directory "~/org/"
