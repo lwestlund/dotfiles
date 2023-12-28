e930cfe9f4c5cda6f1af29b097753d6709e07304
Love Westlund <love.westlund@proton.me>
2023-12-29 00:09:16 +0100

    Add icons to the buffer list

--- init.el
+++ init.el
@@ -72,7 +72,7 @@
         +icons
         +ranger)
        electric          ; smarter, keyword-based electric-indent
-       ibuffer           ; interactive buffer management
+       (ibuffer +icons)  ; interactive buffer management
        undo              ; persistent, smarter undo for your inevitable mistakes
        vc                ; version-control and Emacs, sitting in a tree
 
