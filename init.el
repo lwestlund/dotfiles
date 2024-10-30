97e7bdf17e102980d905e3d9c0e2db80fa846466
Love Westlund <love.westlund@proton.me>
2024-10-30 13:07:55 +0100

    yaml: enable all the things

--- init.el
+++ init.el
@@ -185,7 +185,9 @@
        ;;swift             ; who asked for emoji variables?
        ;;terra             ; Earth and Moon in alignment for performance.
        ;;web               ; the tubes
-       yaml              ; JSON, but readable
+       (yaml            ; JSON, but readable
+        +lsp
+        +tree-sitter)
 
        :os
        ;; macos             ; MacOS-specific commands
