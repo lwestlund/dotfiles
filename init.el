922771ab32d4cf4b677fc47f356b7a034dc9e44b
Love Westlund <love.westlund@proton.me>
2025-03-16 19:51:18 +0100

    python: enable pyright
    This enabled pyright, a static type checker, to make python slightly
    more understandable. If you are good with your type hints, it will tell
    you when you mess up and use an int for a bool parameter, and stuff like
    that.

--- init.el
+++ init.el
@@ -164,6 +164,7 @@
        ;;purescript        ; javascript, but functional
        (python
         +lsp
+        +pyright
         ;; +pyenv
         +tree-sitter)       ; beautiful is better than ugly
        ;;qt                ; the 'cutest' gui framework ever
