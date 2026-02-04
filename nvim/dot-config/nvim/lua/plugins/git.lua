c653b8e47d78f3cc3059da5caf24a2e97ee34e98
Love Westlund <love.westlund@proton.me>
2026-02-04 11:55:09 +0100

    nvim: add diffview plugin

--- nvim/dot-config/nvim/lua/plugins/git.lua
+++ nvim/dot-config/nvim/lua/plugins/git.lua
@@ -4,13 +4,13 @@ return {
     lazy = true,
     dependencies = {
       "nvim-lua/plenary.nvim", -- required
-      "sindrets/diffview.nvim", -- optional - Diff integration
 
       -- Only one of these is needed.
       -- "nvim-telescope/telescope.nvim", -- optional
       -- "ibhagwan/fzf-lua",              -- optional
       -- "nvim-mini/mini.pick",           -- optional
       "folke/snacks.nvim", -- optional
+
       "https://github.com/isakbm/gitgraph.nvim",
     },
     keys = {
@@ -42,6 +42,27 @@ return {
         commit_editor = {
           ["gk"] = "PrevMessage",
           ["gj"] = "NextMessage",
+        },
+      },
+    },
+  },
+
+  {
+    "sindrets/diffview.nvim",
+    cmd = { "DiffviewOpen", "DiffviewFileHistory" },
+    keys = {
+      { "<leader>gL", "<cmd>DiffviewFileHistory %<cr>", desc = "Diffview File History" },
+    },
+    opts = {
+      keymaps = {
+        view = {
+          { "n", "q", "<cmd>DiffviewClose<cr>", { desc = "Close Diffview" } },
+        },
+        file_panel = {
+          { "n", "q", "<cmd>DiffviewClose<cr>", { desc = "Close Diffview" } },
+        },
+        file_history_panel = {
+          { "n", "q", "<cmd>DiffviewClose<cr>", { desc = "Close Diffview" } },
         },
       },
     },
