7078a4fa5200ea9edfffc307d7dbd13dacfce647
Love Westlund <love.westlund@proton.me>
2026-02-04 11:42:54 +0100

    nvim: split ui stuff to ui plugin

---
+++ nvim/dot-config/nvim/lua/plugins/ui.lua
@@ -0,0 +1,60 @@
+return {
+  {
+    "nvim-lualine/lualine.nvim",
+    opts = {
+      theme = "doom-one",
+      sections = {
+        lualine_x = {},
+        lualine_y = {
+          require("snacks").profiler.status(),
+            -- stylua: ignore
+            {
+              function() return require("noice").api.status.command.get() end,
+              cond = function() return package.loaded["noice"] and require("noice").api.status.command.has() end,
+              color = function() return { fg = Snacks.util.color("Statement") } end,
+            },
+            -- stylua: ignore
+            {
+              function() return require("noice").api.status.mode.get() end,
+              cond = function() return package.loaded["noice"] and require("noice").api.status.mode.has() end,
+              color = function() return { fg = Snacks.util.color("Constant") } end,
+            },
+            -- stylua: ignore
+            {
+              function() return "  " .. require("dap").status() end,
+              cond = function() return package.loaded["dap"] and require("dap").status() ~= "" end,
+              color = function() return { fg = Snacks.util.color("Debug") } end,
+            },
+            -- stylua: ignore
+            {
+              require("lazy.status").updates,
+              cond = require("lazy.status").has_updates,
+              color = function() return { fg = Snacks.util.color("Special") } end,
+            },
+          {
+            "diff",
+            symbols = {
+              added = LazyVim.config.icons.git.added,
+              modified = LazyVim.config.icons.git.modified,
+              removed = LazyVim.config.icons.git.removed,
+            },
+            source = function()
+              local gitsigns = vim.b.gitsigns_status_dict
+              if gitsigns then
+                return {
+                  added = gitsigns.added,
+                  modified = gitsigns.changed,
+                  removed = gitsigns.removed,
+                }
+              end
+            end,
+          },
+        },
+        lualine_z = {
+          { "progress", separator = " ", padding = { left = 1, right = 0 } },
+          { "location", padding = { left = 0, right = 1 } },
+        },
+      },
+    },
+  },
+}
