ad87eeb611c7bb1357a0329ce159ca07ab288556
Love Westlund <love.westlund@proton.me>
2026-02-06 20:31:50 +0100

    nvim: clean up trouble from example config

--- nvim/dot-config/nvim/lua/plugins/example.lua
+++ nvim/dot-config/nvim/lua/plugins/example.lua
@@ -147,16 +147,6 @@ end
 -- * disable/enabled LazyVim plugins
 -- * override the configuration of LazyVim plugins
 return {
-  -- change trouble config
-  {
-    "folke/trouble.nvim",
-    -- opts will be merged with the parent spec
-    opts = { use_diagnostic_signs = true },
-  },
-
-  -- disable trouble
-  { "folke/trouble.nvim", enabled = false },
-
   -- override nvim-cmp and add cmp-emoji
   {
     "hrsh7th/nvim-cmp",
