7ad30b925e7ffe76e31828af16c57bf92bad078d
Love Westlund <love.westlund@proton.me>
2026-02-06 20:15:43 +0100

    nvim: add blink keymaps C-j/k for next/prev

---
+++ nvim/dot-config/nvim/lua/plugins/completions.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    "saghen/blink.cmp",
+    opts = {
+      keymap = {
+        preset = "default",
+        ["<C-k>"] = { "select_prev", "fallback" },
+        ["<C-j>"] = { "select_next", "fallback" },
+      },
+      cmdline = {
+        keymap = { preset = "inherit" },
+      },
+    },
+  },
+}
