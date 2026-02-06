42133cdb04f090cb4d657a651e6a5b160b552961
Love Westlund <love.westlund@proton.me>
2026-02-06 13:13:04 +0100

    nvim: when closing last buffer, reopen dashboard

--- nvim/dot-config/nvim/lua/config/autocmds.lua
+++ nvim/dot-config/nvim/lua/config/autocmds.lua
@@ -6,3 +6,16 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+vim.api.nvim_create_autocmd("BufDelete", {
+  callback = function()
+    vim.schedule(function()
+      local bufs = vim.fn.getbufinfo({ buflisted = 1 })
+      if #bufs == 0 or (#bufs == 1 and bufs[1].name == "" and bufs[1].changed == 0) then
+        if _G.Snacks and Snacks.dashboard then
+          Snacks.dashboard.open()
+        end
+      end
+    end)
+  end,
+})
