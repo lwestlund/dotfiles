152619b4de72442a48fbc29054158ea0970df2bd
Love Westlund <love.westlund@proton.me>
2026-02-12 18:48:18 +0100

    nvim: extend todo-comments matching with name or similar
    This should now for example match `TODO(name)` or `TODO(issue-number)`.

--- nvim/dot-config/nvim/lua/plugins/ui.lua
+++ nvim/dot-config/nvim/lua/plugins/ui.lua
@@ -68,4 +68,18 @@ return {
       },
     },
   },
+  {
+    "folke/todo-comments.nvim",
+    opts = {
+      highlight = {
+        -- Use non-greedy matching up to the first keyword of the line, and an optional match of a
+        -- parenthesis after the keyword, with a non-greedy matching of the closing parenthesis.
+        -- Inspired from https://github.com/folke/todo-comments.nvim/pull/255.
+        pattern = [[.{-}<((KEYWORDS)%(\(.{-1,}\))?):]],
+      },
+      search = {
+        pattern = [[\b(KEYWORDS)(\([^\)]*\))?:]],
+      },
+    },
+  },
 }
