47287c3858854c557ede4275b9a923ee48a4e430
Love Westlund <love.westlund@proton.me>
2026-02-22 14:18:15 +0100

    nvim: add lang just
    This is for working with justfiles, see https://just.systems/.

---
+++ nvim/dot-config/nvim/lua/plugins/lang/just.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "just",
+      },
+    },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        just = {},
+      },
+    },
+  },
+}
