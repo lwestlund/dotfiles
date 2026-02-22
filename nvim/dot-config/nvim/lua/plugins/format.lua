ff2cb50e391acb2b150a4ddaa49a30512fa62f39
Love Westlund <love.westlund@proton.me>
2026-02-22 13:54:50 +0100

    nvim(format): enable automatic dioxus rsx! formatting

--- nvim/dot-config/nvim/lua/plugins/format.lua
+++ nvim/dot-config/nvim/lua/plugins/format.lua
@@ -2,6 +2,9 @@ return {
   {
     "stevearc/conform.nvim",
     opts = {
+      formatters_by_ft = {
+        rust = { "rustfmt", "dioxus" },
+      },
       formatters = {
         sqlfluff = {
           args = { "format", "-" },
