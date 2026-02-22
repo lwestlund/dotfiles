6028d9973cf4888b0d6914ec80ba89cec84e3396
Love Westlund <love.westlund@proton.me>
2026-02-22 13:52:23 +0100

    nvim: add details to completions menu

--- nvim/dot-config/nvim/lua/plugins/completions.lua
+++ nvim/dot-config/nvim/lua/plugins/completions.lua
@@ -10,6 +10,28 @@ return {
       cmdline = {
         keymap = { preset = "inherit" },
       },
+      completion = {
+        menu = {
+          draw = {
+            align_to = "label",
+            columns = {
+              { "kind_icon" },
+              { "label", "detail" },
+            },
+            components = {
+              detail = {
+                -- A component that displays details of an item, for example
+                -- a vairable will be detailed with its type, or a function
+                -- by its signature.
+                text = function(item)
+                  return item.item.detail or ""
+                end,
+                -- highlight = "Comment",
+              },
+            },
+          },
+        },
+      },
     },
   },
 }
