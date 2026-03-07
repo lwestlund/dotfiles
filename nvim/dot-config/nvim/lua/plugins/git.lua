dcfaffee8ae10027b5c64ce09ccb6ced307ee3d8
Love Westlund <love.westlund@proton.me>
2026-03-07 11:59:53 +0100

    nvim: replace neogit with lazygit

--- nvim/dot-config/nvim/lua/plugins/git.lua
+++ nvim/dot-config/nvim/lua/plugins/git.lua
@@ -1,46 +1,5 @@
 return {
   {
-    "NeogitOrg/neogit",
-    lazy = true,
-    dependencies = {
-      "nvim-lua/plenary.nvim", -- required
-
-      -- Only one of these is needed.
-      -- "nvim-telescope/telescope.nvim", -- optional
-      -- "ibhagwan/fzf-lua",              -- optional
-      -- "nvim-mini/mini.pick",           -- optional
-      "folke/snacks.nvim", -- optional
-
-      "https://github.com/isakbm/gitgraph.nvim",
-    },
-    keys = {
-      {
-        "<leader>gg",
-        function()
-          require("neogit").open()
-        end,
-        desc = "Neogit status",
-      },
-    },
-    opts = {
-      graph_style = "kitty",
-      remember_settings = false,
-      kind = "tab",
-      mappings = {
-        popup = {
-          ["p"] = "PushPopup",
-          ["P"] = false,
-          ["F"] = "PullPopup",
-        },
-        commit_editor = {
-          ["gk"] = "PrevMessage",
-          ["gj"] = "NextMessage",
-        },
-      },
-    },
-  },
-
-  {
     "sindrets/diffview.nvim",
     cmd = { "DiffviewOpen", "DiffviewFileHistory" },
     keys = {
