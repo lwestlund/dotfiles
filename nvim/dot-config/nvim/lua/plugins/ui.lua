46f29016098ea84bec3e9854353904d778f41ee0
Love Westlund <love.westlund@proton.me>
2026-04-12 22:26:00 +0200

    nvim(lualine): use default config but clear lualine_z
    LazyVim configures lualine to show system time in the z section, which I
    don't want. My previous solution to this was to manually shift sections
    y to z and x to y, and clearing section x. However, this requires me to
    have the same configuration as LazyVim already has, which seems overly
    complicated. A simpler approach is to use whatever LazyVim defines and
    just clear the z section that holds system time.

--- nvim/dot-config/nvim/lua/plugins/ui.lua
+++ nvim/dot-config/nvim/lua/plugins/ui.lua
@@ -15,56 +15,7 @@ return {
             end,
           },
         },
-        lualine_x = {},
-        lualine_y = {
-          require("snacks").profiler.status(),
-          -- stylua: ignore
-          {
-            require("noice").api.status.command.get,
-            cond = require("noice").api.status.command.has,
-            color = function() return { fg = Snacks.util.color("Statement") } end,
-          },
-          -- stylua: ignore
-          {
-            require("noice").api.status.mode.get,
-            cond = require("noice").api.status.mode.has,
-            color = function() return { fg = Snacks.util.color("Constant") } end,
-          },
-          -- stylua: ignore
-          {
-            function() return "  " .. require("dap").status() end,
-            cond = function() return package.loaded["dap"] and require("dap").status() ~= "" end,
-            color = function() return { fg = Snacks.util.color("Debug") } end,
-          },
-          -- stylua: ignore
-          {
-            require("lazy.status").updates,
-            cond = require("lazy.status").has_updates,
-            color = function() return { fg = Snacks.util.color("Special") } end,
-          },
-          {
-            "diff",
-            symbols = {
-              added = LazyVim.config.icons.git.added,
-              modified = LazyVim.config.icons.git.modified,
-              removed = LazyVim.config.icons.git.removed,
-            },
-            source = function()
-              local gitsigns = vim.b.gitsigns_status_dict
-              if gitsigns then
-                return {
-                  added = gitsigns.added,
-                  modified = gitsigns.changed,
-                  removed = gitsigns.removed,
-                }
-              end
-            end,
-          },
-        },
-        lualine_z = {
-          { "progress", separator = " ", padding = { left = 1, right = 0 } },
-          { "location", padding = { left = 0, right = 1 } },
-        },
+        lualine_z = {},
       },
     },
   },
